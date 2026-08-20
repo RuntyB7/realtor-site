3976ffd9c294f29230aea8582b08165df0fdd1f6
MogilnyLeon <mogilnyleon7@gmail.com>
2026-08-19 21:10:59 -0400

    changed some things in vite config for this website version

--- vite.config.ts
+++ vite.config.ts
@@ -154,6 +154,9 @@ const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(
 
 export default defineConfig({
   plugins,
+
+  base: "/",
+  
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
